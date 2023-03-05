668f7d59e7aaa9cf3d55fe56c20359c2d855fddb
noussari-anis <101105166+noussari-anis@users.noreply.github.com>
2023-03-05 14:53:26 +0100

    crud

--- src/main/java/com/pi/Centrale_Achat/repositories/OrederRepo.java
+++ src/main/java/com/pi/Centrale_Achat/repositories/OrederRepo.java
@@ -4,5 +4,11 @@ import com.pi.Centrale_Achat.entities.Bill;
 import com.pi.Centrale_Achat.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrederRepo extends JpaRepository<Order,Integer> {
+    int countOrdersByDatCmdBetween(Date d1, Date d2);
+    Order findOrderByDatCmd(Date d);
+    List<Order> findOrdersByUserId(int id);
 }
